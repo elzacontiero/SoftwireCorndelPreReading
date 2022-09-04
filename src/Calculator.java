6f70d660e0592d3413646bbd55f9a7bfaa9653ee
Elza Contiero <contieroelza@gmail.com>
2022-09-04 13:24:20 +0100

    Add note on hash map.

--- src/Calculator.java
+++ src/Calculator.java
@@ -29,8 +29,9 @@ public class Calculator {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in); // Scanner class to collect input from user.
-        // Adding a HashMap containing all available calculations. The idea is to map user's selection of operators
-        // into instances of calculations.
+        // I am adding a HashMap containing all available calculations.
+        // The idea is to map user's selection of operators into instances of calculations.
+        // This avoids the need of a switch statement that would otherwise grow as we add more operations.
 
         // HashMap will associate the String Key operator to Calculation Interface.(Value)
         HashMap<String, Calculation> operations = new HashMap<>();
